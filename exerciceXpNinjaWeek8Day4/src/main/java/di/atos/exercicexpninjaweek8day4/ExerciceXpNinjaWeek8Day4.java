e8e2b3b1139366e4f19c3aed571f3858a79b9f68
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2023-02-21 08:13:41 +0000

    first commit

---
+++ exerciceXpNinjaWeek8Day4/src/main/java/di/atos/exercicexpninjaweek8day4/ExerciceXpNinjaWeek8Day4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.atos.exercicexpninjaweek8day4;
+
+/**
+ *
+ * @author ezekielkouassi
+ */
+public class ExerciceXpNinjaWeek8Day4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
